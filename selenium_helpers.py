06f757ef562bd9089fd1951c079cffc337e8da55
Henri Immonen <henri.immonen@mostdigital.fi>
2019-09-03 08:43:33 +0300

    Fix pylint warnings
    Signed-off-by: Henri Immonen <henri.immonen@mostdigital.fi>

--- selenium_helpers.py
+++ selenium_helpers.py
@@ -6,12 +6,9 @@
 
 """Script has helper functions for using Selenium."""
 
-import os
-import sys
 import time
 from contextlib import contextmanager
 
-from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 
 import _web_driver
@@ -30,11 +27,6 @@ def get_driver():
 class InvalidXPath(Exception):
     """Custom exception for invalid xpath."""
 
-    def __init__(self, *args, **kwargs):
-        """Initialize 'InvalidXPath'."""
-
-        super().__init__(*args, **kwargs)
-
 def _validate_root_element(xpath, root_element):
     """
     Verify that 'xpath' and 'root_element' are valid and return validated 'root_element'. Otherwise
@@ -44,6 +36,7 @@ def _validate_root_element(xpath, root_element):
     if not root_element:
         root_element = get_driver()
 
+    # pylint: disable=protected-access
     if xpath.startswith("./") and isinstance(root_element, _web_driver._WebDriver):
         raise InvalidXPath("'xpath' starting with: './', requires a 'root_element'")
     if xpath.startswith("//") and not isinstance(root_element, _web_driver._WebDriver):
@@ -63,7 +56,11 @@ def _find_by_xpath(xpath, root_element=None, many=False):
         return root_element.find_elements_by_xpath(xpath)
     return root_element.find_element_by_xpath(xpath)
 
-find_by_xpath = RepeatOnFailure(exception_types=(WebDriverException, ))(_find_by_xpath)
+@RepeatOnFailure(exception_types=(WebDriverException, ))
+def find_by_xpath(xpath, root_element=None, many=False):
+    """Decorated '_find_by_xpath()' call."""
+
+    return _find_by_xpath(xpath, root_element=root_element, many=many)
 
 def _click_by_xpath(xpath, root_element):
     """Find and click the first element matching 'xpath' and 'root_element'."""
@@ -73,7 +70,11 @@ def _click_by_xpath(xpath, root_element):
     element.click()
     return element
 
-click_by_xpath = RepeatOnFailure(exception_types=(WebDriverException, ))(_click_by_xpath)
+@RepeatOnFailure(exception_types=(WebDriverException, ))
+def click_by_xpath(xpath, root_element):
+    """Decorated '_click_by_xpath()' call."""
+
+    return _click_by_xpath(xpath, root_element)
 
 @RepeatOnFailure(exception_types=(WebDriverException, ))
 def read_text_by_xpath(xpath, root_element=None):
@@ -84,15 +85,20 @@ def read_text_by_xpath(xpath, root_element=None):
 
 def _read_attribute_by_xpath(xpath, attribute, root_element=None):
     """
-    Find the first element matching 'xpath' and 'root_element', call its method 'get_attribute(<attribute>)' and return it. 'xpath'
-    and 'root_element' are passed to 'find_by_xpath()'.
+    Find the first element matching 'xpath' and 'root_element', call its method
+    'get_attribute(<attribute>)' and return it. 'xpath' and 'root_element' are passed to
+    'find_by_xpath()'.
     """
 
     root_element = _validate_root_element(xpath, root_element)
     element = _find_by_xpath(xpath, root_element=root_element, many=False)
     return element.get_attribute(attribute)
 
-read_attribute_by_xpath = RepeatOnFailure(exception_types=(WebDriverException, ))(_read_attribute_by_xpath)
+@RepeatOnFailure(exception_types=(WebDriverException, ))
+def read_attribute_by_xpath(xpath, attribute, root_element=None):
+    """Decorated '_read_attribute_by_xpath()' call."""
+
+    return _read_attribute_by_xpath(xpath, attribute, root_element=root_element)
 
 def read_value_by_xpath(xpath, root_element=None):
     """Shorthand for calling 'read_attribute_by_xpath(xpath, "value", ...)'."""
